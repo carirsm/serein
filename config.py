e0a4f431e17c3e56b90df551216dedb6944154ad
Oscar Perez <cari.rsm@gmail.com>
2025-09-01 21:28:46 -0700

    Updated config.py with SQLite fallback

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 load_dotenv()
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
+        f'sqlite:///{os.path.join(basedir, "serein.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.getenv('SECRET_KEY')
