4f686fc46d6afa4a837d24794bf2b8d55feaa8eb
Oscar Perez <cari.rsm@gmail.com>
2025-09-01 22:07:35 -0700

    Updated notes run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=5000)
-# This is the entry point for the Flask application
+# Entry point for the Flask application
