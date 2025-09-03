465e78f2676ae95b9ed0d63a37898d46489091a7
Oscar Perez <cari.rsm@gmail.com>
2025-09-02 17:54:03 -0700

    Cleaned up init module

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,13 @@
 from flask import Flask
-from flask_wtf import CSRFProtect
 from .database import db
 from config import Config
 
-# creates Flask app and configures it to connect to database + connects SQLAlchemy to it
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object(Config)
 
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Disable caching for static files
-
-    # csrf = CSRFProtect(app)  # Initialize CSRF protection
-
-   #  @app.context_processor
-   # def inject_csrf_token():
-    #    from flask_wtf.csrf import generate_csrf
-    #    return dict(csrf_token=generate_csrf)
 
     # setting up to connect to database
 
